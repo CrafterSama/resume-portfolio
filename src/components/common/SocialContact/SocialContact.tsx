5110aabefc140fa8efe5fc425b55a73cc7013ee0
Julmer Olivero <jolivero.03@gmail.com>
2022-04-28 13:17:26 -0400

    Changes in the Footer and the contact info

--- src/components/common/SocialContact/SocialContact.tsx
+++ src/components/common/SocialContact/SocialContact.tsx
@@ -1,4 +1,8 @@
 import { FC } from "react";
+import { MdEmail } from "react-icons/md";
+import { ImMobile, ImTwitter, ImFacebook, ImLinkedin } from "react-icons/im";
+import { FaInstagram, FaGithubAlt } from "react-icons/fa";
+import { SiSkype } from "react-icons/si";
 
 type SocialItems = {
     name: string;
@@ -36,14 +40,14 @@ const SocialContact: FC<SocialContactProps> = ({
     return (
         <div className="overflow-hidden social-box">
             <ul className="container social-contact text-center animated fadeInDown delay-800ms">
-                <li className="animated delay-600ms fadeInDown"><i className="fas fa-envelope"></i>&nbsp;&nbsp; <a href={'mailto:' + email.url}>{email.name}</a></li>
-                <li className="animated delay-800ms fadeInDown"><i className="fas fa-mobile-alt"></i>&nbsp;&nbsp; <a href={'tel:' + mobile.url}>{mobile.name}</a></li>
-                <li className="animated delay-1s fadeInDown"><i className="fab fa-twitter"></i>&nbsp;&nbsp; <a href={twitter.url}>{twitter.name}</a></li>
-                <li className="animated delay-1dot2s fadeInDown"><i className="fab fa-facebook"></i>&nbsp;&nbsp; <a href={facebook.url}>{facebook.name}</a></li>
-                <li className="animated delay-1dot4s fadeInDown"><i className="fab fa-instagram"></i>&nbsp;&nbsp; <a href={instagram.url}>{instagram.name}</a></li>
-                <li className="animated delay-1dot6s fadeInDown"><i className="fab fa-github"></i>&nbsp;&nbsp; <a href={github.url}>{github.name}</a></li>
-                <li className="animated delay-1dot8s fadeInDown"><i className="fab fa-skype"></i>&nbsp;&nbsp; <a href={skype.url}>{skype.name}</a></li>
-                <li className="animated delay-2s fadeInDown"><i className="fab fa-linkedin-in"></i>&nbsp;&nbsp; <a href={linkedin.url}>{linkedin.name}</a></li>
+                <li className="animated delay-600ms fadeInDown"><MdEmail className="icons" size="24" color="white" />&nbsp;&nbsp; <a href={'mailto:' + email.url}>{email.name}</a></li>
+                <li className="animated delay-800ms fadeInDown"><ImMobile className="icons" size="24" color="white" />&nbsp;&nbsp; <a href={'tel:' + mobile.url}>{mobile.name}</a></li>
+                <li className="animated delay-1s fadeInDown"><ImTwitter className="icons" size="24" color="white" />&nbsp;&nbsp; <a href={twitter.url}>{twitter.name}</a></li>
+                <li className="animated delay-1dot2s fadeInDown"><ImFacebook className="icons" size="24" color="white" />&nbsp;&nbsp; <a href={facebook.url}>{facebook.name}</a></li>
+                <li className="animated delay-1dot4s fadeInDown"><FaInstagram className="icons" size="24" color="white" />&nbsp;&nbsp; <a href={instagram.url}>{instagram.name}</a></li>
+                <li className="animated delay-1dot6s fadeInDown"><FaGithubAlt className="icons" size="24" color="white" />&nbsp;&nbsp; <a href={github.url}>{github.name}</a></li>
+                <li className="animated delay-1dot8s fadeInDown"><SiSkype className="icons" size="24" color="white" />&nbsp;&nbsp; <a href={skype.url}>{skype.name}</a></li>
+                <li className="animated delay-2s fadeInDown"><ImLinkedin className="icons" size="24" color="white" />&nbsp;&nbsp; <a href={linkedin.url}>{linkedin.name}</a></li>
             </ul>
         </div>
     )
